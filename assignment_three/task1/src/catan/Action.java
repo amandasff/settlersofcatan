8e9c2ac752cf173f6762e3fe779ec40524d19fba
alish-saqlain <saqlaina@mcmaster.ca>
2026-03-18 16:32:44 -0400

    starting the command design pattern with undo redo

--- assignment_three/task1/src/catan/Action.java
+++ assignment_three/task1/src/catan/Action.java
@@ -3,5 +3,6 @@ package catan;
 public interface Action {
     boolean isExecutable(GameState state, Player player);
     void execute(GameState state, Player player);
+    void undo(GameState state, Player player);
     String describe();
 }
